9430830475b055baa8746b284e6828b0c61f6974
Lucas Exequiel <arrejoria1994@gmail.com>
2023-10-01 20:57:38 -0300

    Update Scripts - Storage URLS

--- js/main.js
+++ js/main.js
@@ -8,11 +8,13 @@ const selectElement = (selector) => {
 
 const ORIGINAL_URL = "https://bru-app.vercel.app/";
 const API_BASE = "https://api.shrtco.de/v2/";
-const SHORTEN_URL = `${API_BASE}shorten?url=${ORIGINAL_URL}`;
-// const SHORTEN_URL = `${API_BASE}shorten?url=${encodeURIComponent(ORIGINAL_URL)}`;
-
+// const SHORTEN_URL = `${API_BASE}shorten?url=${ORIGINAL_URL}`;
+const SHORTEN_URL = `https://api.shrtco.de/v2/shorten?url=${ORIGINAL_URL}`;
+const urlList = selectElement('#urlList');
 const form =selectElement('#shortForm');
 const inpUrl = selectElement('input[name="shortener"]')
+const copyButton = selectElement('.url__button');
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log('form submited')
@@ -21,15 +23,61 @@ form.addEventListener('submit', (e) => {
     shortenURL(url)
 })
 
-async function shortenURL(url){
-
+window.addEventListener('DOMContentLoaded', () => {
+    showURLS()
+})
+async function shortenURL(url) {
     try {
-        const res = await fetch(SHORTEN_URL)
-        const data = res.json();
-    
-        console.log(data)
+        const res = await fetch(`${API_BASE}shorten?url=${url}`);
+        if (!res.ok) {
+            throw new Error(`Error al acortar la URL: ${res.statusText}`);
+        }
+        const data = await res.json();
+        if (data.ok) {
+            storageURL(data)
+
+        } else {
+            throw new Error(`Error al acortar la URL: ${data.error_code}`);
+        }
     } catch (error) {
         console.error(error);
     }
-
 }
+
+
+function showURLS() {
+    const storedURLS = JSON.parse(localStorage.getItem('dataURL'));
+
+    storedURLS.forEach((el) => {
+        const itemList = `
+        <li class="shortener__item url col-11 col-md-11 col-lg-10">
+            <span class="url__large">${el.large_url}</span>
+            <span class="url__short">${el.short_url}</span>
+            <button class="url__button" type="button">Copy</button>
+        </li>`;
+    
+        urlList.insertAdjacentHTML('afterbegin', itemList);
+    })
+}
+
+function storageURL(data) {
+    // Obtener la lista de objetos almacenados previamente, o crear una nueva lista si no existe.
+    const storedList = JSON.parse(localStorage.getItem('dataURL')) || [];
+
+    const dataURL = {
+        "code": data.result.code,
+        "large_url": data.result.original_link,
+        "short_url": data.result.full_short_link,
+    };
+
+    storedList.push(dataURL);
+
+    localStorage.setItem('dataURL', JSON.stringify(storedList));
+}
+
+
+function handleCopy(){
+    
+}
+
+// copyButton.addEventListener()
